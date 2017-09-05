c7e794df40bd45af34a994cddbc1c8117e8d70e9
Yun Hao (郝运) <haoyun_public@163.com>
2017-09-05 23:47:15 +0200

    add highlight.js

--- src/site.hs
+++ src/site.hs
@@ -4,7 +4,6 @@ import           Data.Monoid (mappend)
 import           Hakyll
 
 import           Data.List        (isPrefixOf, isSuffixOf)
-import           System.FilePath  (takeFileName)
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith hfConfiguration $ do
@@ -75,20 +74,10 @@ hfConfiguration = defaultConfiguration
 --    , storeDirectory       = "_cache"
 --    , tmpDirectory         = "_cache/tmp"
         providerDirectory    = "./src_site"
-      , ignoreFile           = ignoreFile'
+--    , ignoreFile           = ignoreFile'
 --    , deployCommand        = "echo 'No deploy command specified' && exit 1"
 --    , deploySite           = system . deployCommand
 --    , inMemoryCache        = True
 --    , previewHost          = "127.0.0.1"
 --   , previewPort          = 8000
     }
-  where
-    ignoreFile' path
-        | "Foundation"    `isPrefixOf` fileName = True
-        | "."    `isPrefixOf` fileName = True
-        | "#"    `isPrefixOf` fileName = True
-        | "~"    `isSuffixOf` fileName = True
-        | ".swp" `isSuffixOf` fileName = True
-        | otherwise                    = False
-      where
-        fileName = takeFileName path
