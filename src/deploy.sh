e0b7218629c1eb095d94f740f92529c73c612a09
Yun Hao (郝运) <haoyun_public@163.com>
2017-09-07 04:08:25 +0200

    source update

---
+++ src/deploy.sh
@@ -0,0 +1,18 @@
+#/bin/zsh
+
+chcp 65001
+cd Foundation/ &&
+foundation build &&
+cd ../
+cabal run rebuild &&
+git clean -xf &&
+rsync -avr --delete --exclude='.git' _site/ html_public/
+cd html_public
+git add .
+# git commit -m 'pages update'    # use the next if you do not want to keep
+git commit --amend --no-edit  # any history of generated pages   
+git push origin HEAD:master -f
+cd ..
+git add .
+git commit -m 'source update'
+git push origin source
