2e6078c76dfea5231e6e51c64809576d0ce7482b
Yun Hao (郝运) <haoyun_public@163.com>
2017-09-09 03:53:02 +0200

    source update

--- src_site/js/app.js
+++ src_site/js/app.js
@@ -50,24 +50,3 @@ function fadeToMark() {
 function checkItOut() {
   CHECK.prop('checked', true);
 }
-
-
-
- /*
- /
- /
- /
- /
- / clicky
- /
- /
- /
- /
- /
- /
- /
- /
-*/
-<script src="//static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">try{ clicky.init(101019144); }catch(e){}</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101019144ns.gif" /></p></noscript>
