c9e25b9207dced100839dedb586d55ae154c6cf2
Yun Hao (郝运) <haoyun_public@163.com>
2017-09-11 01:01:55 +0200

    source update

--- src_site/js/app.js
+++ src_site/js/app.js
@@ -15,4 +15,6 @@ c_container.click(function() {
   $('.c-checkbox-4').addClass("c-checkbox-checkmark");//.delay(3000).queue(function() {
   //  $('.c-checkbox-5').addClass("c-checkbox-checked");
   //});
+  $('#to-activate-1').contents().unwrap().wrapAll("<a href='/s/'></a>");
+  $('#to-activate-1').contents().unwrap().wrapAll("<a href='/n/'></a>");
 });
