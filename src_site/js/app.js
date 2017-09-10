f913d7ad8279880ffe8e63ab7229a403fff60d5a
Yun Hao (郝运) <haoyun_public@163.com>
2017-09-11 01:06:40 +0200

    source update

--- src_site/js/app.js
+++ src_site/js/app.js
@@ -8,13 +8,12 @@
 var c_container = $(".c-container");
 
 c_container.click(function() {
-  $('.c-checkbox-0').remove();
-  $('.c-checkbox-1').addClass("c-checkbox-borderAnimation-33");
-  $('.c-checkbox-3').addClass("c-checkbox-spinnerAnimation");
-  $('.c-checkbox-2').addClass("c-checkbox-spinner");
-  $('.c-checkbox-4').addClass("c-checkbox-checkmark");//.delay(3000).queue(function() {
-  //  $('.c-checkbox-5').addClass("c-checkbox-checked");
-  //});
-  $('#to-activate-1').contents().unwrap().wrapAll("<a href='/s/'></a>");
-  $('#to-activate-1').contents().unwrap().wrapAll("<a href='/n/'></a>");
+    $('.c-checkbox-0').remove();
+    $('.c-checkbox-1').addClass("c-checkbox-borderAnimation-33");
+    $('.c-checkbox-3').addClass("c-checkbox-spinnerAnimation");
+    $('.c-checkbox-2').addClass("c-checkbox-spinner");
+    $('.c-checkbox-4').addClass("c-checkbox-checkmark").delay(3000).queue(function() {
+        $('#to-activate-1').contents().unwrap().wrapAll("<a href='/s/'></a>");
+        $('#to-activate-2').contents().unwrap().wrapAll("<a href='/n/'></a>");   
+    });
 });
