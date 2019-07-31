f8013e712f1f083056a11dbd477419f9168a520c
HAO, Yun (郝运） <haoyun_public@163.com>
2019-07-31 20:47:14 +0200

    add language switcher

--- src_site/js/app.js
+++ src_site/js/app.js
@@ -44,3 +44,51 @@ jQuery('#rocketmeluncur').click(function() {
         self.className = 'showrocket';
     }, 800);
 });
+
+
+//=============================================================================
+//
+// https://stackoverflow.com/a/43033380/2929058
+//
+//=============================================================================
+
+var lang_de = document.querySelectorAll('.lang-de');
+var lang_en = document.querySelectorAll('.lang-en');
+var lang_cn = document.querySelectorAll('.lang-cn');
+var lang_fr = document.querySelectorAll('.lang-fr');
+var html_node = document.querySelectorAll('html')[0]
+
+function switch_lang (lang) {
+    if (lang == "de") {
+        lang_en.forEach( i => i.style.display = "none");
+        lang_cn.forEach( i => i.style.display = "none");
+        lang_fr.forEach( i => i.style.display = "none");
+        lang_de.forEach( i => i.style.display = "initial");
+        html_node.setAttribute('lang', 'de')
+    }
+    if (lang == "en") {
+        lang_de.forEach( i => i.style.display = "none");
+        lang_cn.forEach( i => i.style.display = "none");
+        lang_fr.forEach( i => i.style.display = "none");
+        lang_en.forEach( i => i.style.display = "initial");
+        html_node.setAttribute('lang', 'en')
+    }
+    if (lang == "cn") {
+        lang_de.forEach( i => i.style.display = "none");
+        lang_en.forEach( i => i.style.display = "none");
+        lang_fr.forEach( i => i.style.display = "none");
+        lang_cn.forEach( i => i.style.display = "initial");
+        html_node.setAttribute('lang', 'zh') // note it is 'zh'!
+    }
+    if (lang == "fr") {
+        lang_de.forEach( i => i.style.display = "none");
+        lang_en.forEach( i => i.style.display = "none");
+        lang_cn.forEach( i => i.style.display = "none");
+        lang_fr.forEach( i => i.style.display = "initial");
+        html_node.setAttribute('lang', 'fr')
+    }
+};
+
+switch_lang("de")
+
+//==============================================================================
